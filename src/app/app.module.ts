e17f2d8adb7a4b4c32de3016f368f894d4b638f9
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-07 13:50:20 -0300

    criando a estrutura do projeto, header, main e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
