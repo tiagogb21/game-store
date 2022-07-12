ce27a6ecf585ca969c5147354c95c06d91fb5134
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-12 18:36:27 -0300

    add data

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -5,3 +5,5 @@ export const LOAD_PRODUCTS = '[Products] Load Products';
 export const LOAD_PRODUCTS_FAIL = '[Products] Load Products Fail';
 
 export const LOAD_PRODUCTS_SUCCESS = '[Products] Load Products Success';
+
+export const LOAD = 'load';
